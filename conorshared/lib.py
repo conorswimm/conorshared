b1fa1fe4dc85171856b009df36fa2ded44f22882
Conor Swimm <conor.swimm@gmail.com>
2022-01-29 16:25:20 +0100

    Initial Push

---
+++ conorshared/lib.py
@@ -0,0 +1,7 @@
+def meatloaf():
+    print('on a hot summer night would you offer your throat to the wolf with the red roses?')
+    x = input()
+    if x == 'would he offer me his mouth?':
+        print('yes')
+    else:
+        print('no')
